d95683251220724384360bcb678e8c459acd93f9
miyazaki ryosuke <tmcit.miyazaki@gmail.com>
2014-12-22 16:18:10 +0900

    cross grade with conll eval

--- cross_validation.sh
+++ cross_validation.sh
@@ -99,6 +99,10 @@ for i in 0 1 2 3 4 5 6 7 8 9
 t1=`date +%s`
 echo "-------cross grade---------"
 python scripts/cross_grade.py ./dumps/eval.* > evaluations/cross_evaluation.txt
+python scripts/conll_calc.py ./conllevals/* > conllevals/cross_evaluation.txt
+
 t2=`date +%s`
 echo `expr $t2 - $t1`sec
+
+
 
