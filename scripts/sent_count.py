5f8684dfc27674ba20a94447052ae2b1ece6e876
miyazaki ryosuke <tmcit.miyazaki@gmail.com>
2015-01-07 19:31:28 +0900

    update: can open multiple files

--- scripts/sent_count.py
+++ scripts/sent_count.py
@@ -2,13 +2,16 @@ import sys
 
 count = 0
 continuity_flag = False
-for line in open(sys.argv[1]):
-    if line.strip() == "":
-        if not continuity_flag:
-            count += 1
-        continuity_flag = True
-        continue
-    continuity_flag = False
+files = sys.argv[1:]
+
+for f in files:
+    for line in open(f):
+        if line.strip() == "":
+            if not continuity_flag:
+                count += 1
+            continuity_flag = True
+            continue
+        continuity_flag = False
 
 print count
 
