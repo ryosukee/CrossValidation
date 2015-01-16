caee3a711c3d96fb936ae8e8c7a745798c528682
miyazaki ryosuke <tmcit.miyazaki@gmail.com>
2015-01-16 16:48:57 +0900

    fix macro micro -> avarage

--- scripts/conll_calc.py
+++ scripts/conll_calc.py
@@ -3,13 +3,10 @@ import sys
 python conll_calc.py [conll_evaled1] [conll_evaled2] ...
 """
 
-#TODO maicro
 d = dict()
 sumnum = len(sys.argv[1:])
 for f in sys.argv[1:]:
     for line in open(f):
-        if line.startswith("processed"):
-            continue
         spl = line.strip().split(";")
         # all
         if line.startswith("accuracy"):
@@ -17,12 +14,21 @@ for f in sys.argv[1:]:
             pre = float(spl[1].split()[-1].split("%")[0])
             rec = float(spl[2].split()[-1].split("%")[0])
             f = float(spl[3].split()[-1].split("%")[0])
+            tc = int(correct*100/pre)
+
 
             d[0] = d.get(0, dict())
             d[0]["Accuracy"] = d[0].get("Accuracy", float()) + acc
             d[0]["Precision"] = d[0].get("Precision", float()) + pre
             d[0]["Recall"] = d[0].get("Recall", float()) + rec
             d[0]["F-value"] = d[0].get("F-value", float()) + f
+            d[0]["Total_Correct"] = d[0].get("Total_Correct", int()) + tc
+            continue
+        elif line.startswith("processed"):
+            total = int(spl[1].split()[-2])
+            correct = int(spl[-1].split()[-1][:-1])
+            d[0]["Total"] = d[0].get("Total", int()) + total
+            d[0]["Correct"] = d[0].get("Correct", int()) + correct
             continue
         # each 
         else:
@@ -42,7 +48,7 @@ ac = ("%.2f" % (d[0]["Accuracy"]/sumnum)).rjust(6)
 pr = ("%.2f" % (d[0]["Precision"]/sumnum)).rjust(6)
 re = ("%.2f" % (d[0]["Recall"]/sumnum)).rjust(6)
 f =  ("%.2f" % (d[0]["F-value"]/sumnum)).rjust(6)
-print "Maccro"
+print "Average"
 print "Accuracy: %s%%; Precision: %s%%; Recall: %s%%; F: %s" % (ac, pr, re, f)
 
 del d[0]
